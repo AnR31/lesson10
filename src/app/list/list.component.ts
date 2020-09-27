04374e6f6c2a7fd0dd54df49a43e6117a11dd294
Roman K <roman.k.person@gmail.com>
2020-09-28 00:15:47 +0300

    _

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css']
 })
-export class ListComponent implements OnInit {
+export class ListComponent implements OnInit, HttpInterceptor  {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // this.router.navigate(['/123']);
+    console.log('hello');
+    return of(undefined);
   }
 
 }
